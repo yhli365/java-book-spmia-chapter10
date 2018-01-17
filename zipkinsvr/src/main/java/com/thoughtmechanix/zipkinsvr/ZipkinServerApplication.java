04553405e1d39185143cbdf13fbe49a463a45a08
yhli365 <yhli365@126.com>
2018-01-17 15:18:13 +0800

    docker fixed

--- zipkinsvr/src/main/java/com/thoughtmechanix/zipkinsvr/ZipkinServerApplication.java
+++ zipkinsvr/src/main/java/com/thoughtmechanix/zipkinsvr/ZipkinServerApplication.java
@@ -2,10 +2,12 @@ package com.thoughtmechanix.zipkinsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZipkinServer
 public class ZipkinServerApplication {
 	public static void main(String[] args) {
